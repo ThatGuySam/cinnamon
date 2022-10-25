5c3eb3d650064fa1f742eea43893b04ce27d040f
Sam Carlton <sam@sam.lc>
2022-10-25 14:33:05 -0500

    Setup initial Synonyms UI

--- src/components/builder.vue
+++ src/components/builder.vue
@@ -1,5 +1,16 @@
 <script>
+import getSynonyms from 'synonyms'
 import { messageStorage } from '../helpers/storage.js'
+
+function findSynonyms(word) {
+    try {
+        return getSynonyms(word).n
+    }
+    catch (error) {
+        console.info('Error finding synonyms for', word)
+        return []
+    }
+}
 
 export default {
     data() {
@@ -7,18 +18,48 @@ export default {
             loaded: false,
             phoneNumber: '',
             body: '',
-            messageProviders: [
-                { id: 1, name: 'SMS', value: true },
-                { id: 1, name: 'Whatsapp' },
-                { id: 2, name: 'Telegram' },
-                { id: 3, name: 'Facebook' },
-                { id: 4, name: 'WeChat' },
-                { id: 5, name: 'Slack' },
-                { id: 6, name: 'Discord' },
-                { id: 7, name: 'Line' },
-                // { id: 1, name: 'Signal' },
-            ],
         }
+    },
+    computed: {
+        synonyms() {
+            const words = this.body.split(' ')
+
+            const synonyms = words.map((word) => {
+                // Skip empty words
+                if (word === '') return []
+
+                const wordSynomyms = findSynonyms(word)
+
+                console.log({ wordSynomyms })
+
+                return wordSynomyms
+            })
+
+            return synonyms.flat()
+        },
+
+        synonymsString() {
+            // If there are no words yet
+            // return an empty string
+            if (this.body.trim().length === 0) return ''
+
+            // If there are no synonyms, return none found
+            if (this.synonyms.length === 0) return 'No synonyms found'
+
+            return this.synonyms.join(', ')
+        },
+
+    },
+    mounted() {
+        messageStorage.getLatestMessage()
+            .then((message) => {
+                this.phoneNumber = message.number
+                this.body = message.body
+
+                // Set state to loaded
+                // so that our UI is revealed
+                this.loaded = true
+            })
     },
     methods: {
         // log(event) {
@@ -27,7 +68,7 @@ export default {
         //         value: event.target.value,
         //     })
         // },
-        updateMessage ( propertyName, value ) {
+        updateMessage(propertyName, value) {
             this[propertyName] = value
 
             // Update message storage
@@ -35,23 +76,10 @@ export default {
             // available when the user
             // next loads the page
             messageStorage.setLatestMessage({
-                number: this.phoneNumber,
                 body: this.body,
             })
         },
     },
-    mounted () {
-        messageStorage.getLatestMessage()
-            .then( message => {
-                console.log({ message })
-                this.phoneNumber = message.number
-                this.body = message.body
-
-                // Set state to loaded
-                // so that our UI is revealed
-                this.loaded = true
-            })
-    }
 }
 </script>
 
@@ -62,69 +90,41 @@ export default {
     >
         <div
             class="flex gap-3 text-2xl font-bold"
-        ><div class="animate-spin">߷</div>Loading...</div>
+        >
+            <div class="animate-spin">
+                ߷
+            </div>Loading...
+        </div>
     </div>
     <div
         v-else
         class="builder-container container flex flex-col md:flex-row gap-6 max-w-5xl mx-auto"
     >
-        <div class="builder-device-container">
-            <!-- phoneNumber: {{ phoneNumber }}
-            message: {{ message }} -->
-            <new-message
-                :recipient="phoneNumber"
-                :body="body"
-
-                @update:recipient="updateMessage('phoneNumber', $event)"
-                @update:body="updateMessage('body', $event)"
+        <div
+            class="rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600 w-full"
+        >
+            <label for="name" class="block text-xs font-medium">Words</label>
+            <textarea
+                v-model="body"
+                type="text"
+                name="name"
+                class="block w-full border-0 p-0 placeholder-gray-500 focus:ring-0 sm:text-sm bg-transparent"
+                placeholder="Paste words here"
             />
         </div>
 
-        <div class="options container space-y-6 p-6">
-            <div
-                id="options"
-                class="text-xl font-light"
-            >
-                Options
-            </div>
-
-            <fieldset class="pointer-events-none px-6">
-                <legend class="flex items-center gap-2 text-lg font-medium">
-                    Messaging Providers
-                </legend>
-                <div class="relative mt-4 border-gray-200 divide-y divide-gray-500">
-                    <div
-                        v-for="(provider, providerIdx) in messageProviders"
-                        :key="providerIdx"
-                        class="opacity-25 relative flex items-start py-2"
-                    >
-                        <div class="min-w-0 flex-1 text-sm">
-                            <label :for="`provider-${provider.id}`" class="font-medium select-none">{{ provider.name }}</label>
-                        </div>
-                        <div class="ml-3 flex items-center h-5">
-                            <input
-                                :id="`provider-${provider.id}`"
-                                :name="`provider-${provider.id}`"
-                                type="checkbox"
-                                class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                                :checked="provider?.value"
-                            >
-                        </div>
-                    </div>
-
-                    <div
-                        class="absolute inset-0 flex flex-col justify-center items-center gap-3 px-6"
-                        style="backdrop-filter: blur(1px);"
-                    >
-                        <div class="bg-gray-500 rounded-full px-2 text-center text-sm">
-                            Providers Coming Soon
-                        </div>
-                        <a href="https://forms.gle/hcSthreX1xdWdoJPA" class="text-xs text-blue-500">
-                            <button class="border border-blue-500 rounded px-2">Join Waitlist</button>
-                        </a>
-                    </div>
-                </div>
-            </fieldset>
+        <div
+            class="rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600 w-full"
+        >
+            <label for="name" class="block text-xs font-medium">Synonyms </label>
+            <textarea
+                :value="synonymsString"
+                type="text"
+                name="name"
+                class="block w-full border-0 p-0 placeholder-gray-500 focus:ring-0 sm:text-sm bg-transparent"
+                placeholder=""
+                readonly
+            />
         </div>
     </div>
 </template>
